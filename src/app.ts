92a0247b7719c812eddf1bb2db398371db1109ce
P-drop <pedrorincon.tec@gmail.com>
2026-06-17 20:37:02 +0200

    Refactorización: extraer la instancia de app a src/app.ts para hacerla testeable

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import { getAllGoals, getGoalById } from './controllers/goalController.js';
+
+export const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/goals", getAllGoals);
+
+app.get("/goals/:id", getGoalById)
