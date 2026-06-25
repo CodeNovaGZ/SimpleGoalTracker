b3439571ca21ca90df6ffd4f016b0f60284121dd
sheyito27 <focusdanirico@gmail.com>
2026-06-25 09:07:20 +0200

    Refactorizar app.ts implementando un archivo routes.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import { createGoal, deleteGoal, getAllGoals, getGoalById, updateGoal } from './controllers/goalController.js';
+import { goalRouter } from './routes.js';
 
 export const app = express();
 app.use(cors());
+
+app.use('/goals', goalRouter)
+
 app.use(express.json());
 
-app.get("/goals", getAllGoals);
-
-app.get("/goals/:id", getGoalById)
-
-app.post("/goals/", createGoal)
-
-app.patch("/goals/:id", updateGoal)
-
-app.delete("/goals/:id", deleteGoal)
