d8679919f6e380ac2955fb0c32e1a5f363270b6b
sheyito27 <focusdanirico@gmail.com>
2026-06-25 22:51:44 +0200

    Implementar get en taskController

---
+++ src/controllers/taskController.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from 'express';
+import { z } from 'zod';
+import { taskRepository } from '../repositories/taskRepository.js';
+import { TaskBase } from '../schemas/taskSchema.js';
+import { GoalSchema, UpdateGoalSchema } from '../schemas/goalSchema.js';
+import { goalRepository } from '../repositories/goalRepository.js';
+
+// Modificación en todos los handlers-> Añádir asincronía
+
+// Obtener todas las metas
+export const getAllTasks = async (req: Request, res: Response) => {
+    console.log("Alguien pidió acceso a tasks");
+    try {
+        const tasks = await taskRepository.findAll();
+        res.json(tasks); 
+    }  catch (error) {
+        console.error('Error al obtener metas', error)
+        res.status(500).json({ message: 'Error interno del servidor' })
+    }
+
+};
+
+// Obtener una meta, filtrando por ID
+export const getTaskById = async (req: Request, res: Response) => {
+    try{
+        const task = await goalRepository.findOne(req.params.id);
+        console.log("Alguien pidió acceso a la meta " + task?.title);
+        if (task) return res.status(200).json(task);
+        return res.status(404).json({ message: "Meta no encontrada" });
+    } catch (error) {
+        console.error('Error al obtener la meta', error);
+        return res.status(500).json({ message: 'Error interno del servidor' })
+    } 
+};
+
+// Crear una nueva meta
+export const createGoal = async (req: Request, res: Response) => {
+    console.log("Alguien pidió añadir una meta");
+    const goal = GoalSchema.safeParse(req.body);
+
+    // Comprobar que el body contenga un json que se pueda conbvertir en Goal
+    if (!goal.success) return res.status(400).json({ errors: goal.error.format() });
+
+    try {
+        const newGoal = await goalRepository.addOne(goal.data);
+        console.log(`La meta ${goal.data.id} ha sido añadida`)
+        return res.status(201).json(newGoal);
+
+    } catch (error: any) {
+        if (error?.code === 'P2002') return res.status(409).json({ message: 'Ya existe una meta con ese título' });
+        console.error('Error al crear la meta', error);
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
+
+// Modificar datos de una meta
+export const updateGoal = async (req: Request, res: Response) => {
+    console.log("Alguien pidió modificar una meta");
+    const {id} = req.params;
+    const updates = UpdateGoalSchema.safeParse(req.body)
+    if (!updates.success) return res.status(400).json({ errors: updates.error.format() });
+    try {
+        const updatedGoal = await goalRepository.updateOne(id, updates.data);
+        if (!updatedGoal) return res.status(404).json({ message: "Meta no encontrada" });
+        console.log(`La meta ${id} ha sido modificada`);
+        return res.status(200).json(updatedGoal);
+    } catch (error) {
+        console.error('Error al modificar la meta', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
+
+// Borrar meta
+export const deleteGoal = async (req: Request, res: Response) => {
+    console.log("Alguien pidió borrar una meta");
+    try {
+        const deletedGoal = await goalRepository.deleteOne(req.params.id);
+        if (!deleteGoal) return res.status(404).json({ message: 'Meta no encontrada' });
+        return res.status(200).json(deletedGoal);
+    } catch (error) {
+        console.error('Error al borrar la meta', error);
+        res.status(500).json({ message: 'Error interno del servidor' })
+    }
+};
